f0771470940855a0f71635f1cc40b8b106e3316e
Siisti-Kissa <halonenveikka@gmail.com>
2026-03-16 12:12:56 +0200

    change electron screensize

--- kiinteistotiekartta/electron/main.ts
+++ kiinteistotiekartta/electron/main.ts
@@ -2,13 +2,9 @@ import { app, BrowserWindow } from "electron";
 import path from "node:path";
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 900,
-    height: 700,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
+  const win = new BrowserWindow({ show: false});
+    win.maximize();
+    win.show();
 
   if (process.env.VITE_DEV_SERVER_URL) {
     win.loadURL(process.env.VITE_DEV_SERVER_URL);
