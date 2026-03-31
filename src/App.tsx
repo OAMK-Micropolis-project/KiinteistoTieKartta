ef948ec07b2720f5f9c61df0bddaeb6de2a2144c
Nilssoni <martikainen93@gmail.com>
2026-03-31 16:37:53 +0300

    Radar chart moved to the bottom on detailView & styling updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import AnalyticsView from "./pages/analyticsView.tsx";
+import { HashRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import AnalyticsView from "./pages/AnalyticsView.tsx";
 import DetailView from "./pages/detailView";
 import './App.css'
 import './utils/chartSetup'
