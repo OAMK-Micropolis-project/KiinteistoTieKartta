2de77b72dc2045e99d521f6b073ee7d47805c57e
Andy <andy0ph@gmail.com>
2026-04-06 21:42:09 +0300

    fixed app

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Layout from "./components/Layout";
 import AddProp from "./pages/AddProp";
 import DonutChart from "./components/charts/DonutChart";
 import { useKiinteistot } from "./context/useKiinteistot.ts";
+
 const summaryBoxes = [
   { name: "KIINTEISTÖJÄ", value: "NaN" },
   { name: "KOKONAISPINTA-ALA", value: "NaN" },
@@ -15,18 +16,48 @@ const summaryBoxes = [
   { name: "YLLÄPITÖKULUT / V", value: "NaN" },
   { name: "VUOKRATULOT / V", value: "NaN" },
 ];
-// const realEstates =[
-//   {estateName: "esim tooooooooooooooooooooooooooooooooooooooooooodellla pitkä nimi", portfolio: "A", points: 200, area: "1000 m^2", balanceValue: "1,9M€", path: "detail/1"},
-//   {estateName: "esim 2", portfolio: "B", points: 200, area: "1000 m^2", balanceValue: "1,9M€", path: "detail/2"},
-//   {estateName: "esim 3", portfolio: "C", points: 200, area: "1000 m^2", balanceValue: "1,9M€", path: "detail/3"},
-//   {estateName: "esim 4", portfolio: "D", points: 200, area: "1000 m^2", balanceValue: "1,9M€", path: "detail/4"},
+
+// const realEstates = [
+//   {
+//     estateName: "esim todella pitkä nimi",
+//     portfolio: "A",
+//     points: 200,
+//     area: "1000 m²",
+//     balanceValue: "1,9M€",
+//     path: "detail/1",
+//   },
+//   {
+//     estateName: "esim 2",
+//     portfolio: "B",
+//     points: 200,
+//     area: "1000 m²",
+//     balanceValue: "1,9M€",
+//     path: "detail/2",
+//   },
+//   {
+//     estateName: "esim 3",
+//     portfolio: "C",
+//     points: 200,
+//     area: "1000 m²",
+//     balanceValue: "1,9M€",
+//     path: "detail/3",
+//   },
+//   {
+//     estateName: "esim 4",
+//     portfolio: "D",
+//     points: 200,
+//     area: "1000 m²",
+//     balanceValue: "1,9M€",
+//     path: "detail/4",
+//   },
 // ];
+
 function HomePage() {
   const realEstates = useKiinteistot().kiinteistot;
-
   return (
     <>
       <span className="title">Kiinteistösalkku</span>
+
       <div className="boxesContainer">
         {summaryBoxes.map((box, i) => (
           <div key={i} className="box">
@@ -35,9 +66,11 @@ function HomePage() {
           </div>
         ))}
       </div>
+
       <div className="secondRowContainer">
         <div className="realEstatesContainer">
           <span className="realEstateTitle2">KAIKKI KIINTEISTÖT</span>
+
           <div className="realEstateRowTitles">
             <span className="realEstateTitle2">NIMI</span>
             <span className="realEstateTitle">SALKKU</span>
@@ -45,28 +78,39 @@ function HomePage() {
             <span className="realEstateTitle">PINTA-ALA</span>
             <span className="realEstateTitle">TASEARVO</span>
           </div>
+
+          {realEstates.map((estate, i) => (
+            <NavLink
+              key={i}
+              to={estate.id.toString()}
+              className={`realEstateRow ${
+                estate.oma_salkku === "A"
+                  ? "portfolioA"
+                  : estate.oma_salkku === "B"
+                  ? "portfolioB"
+                  : estate.oma_salkku === "C"
+                  ? "portfolioC"
+                  : estate.oma_salkku === "D"
+                  ? "portfolioD"
+                  : "portfolioDefault"
+              }`}
+            >
+              <span className="estateName">{estate.nimi}</span>
+              <span className="portfolio">{estate.oma_salkku}</span>
+              <span className="estateNumber">{estate.tasearvo[2023]}</span>
+              <span className="estateNumber">{estate.pinta_ala} m²</span>
+              <span className="estateNumber">{estate.tasearvo["2023"]}</span>
+            </NavLink>
+          ))}
         </div>
-        {realEstates.map((estate, i) => (
-          <NavLink key={i} to={`detail/${estate.id.toString()}`}
-            className={`realEstateRow ${estate.oma_salkku === "A" ? "portfolioA" :
-                estate.oma_salkku === "B" ? "portfolioB" :
-                  estate.oma_salkku === "C" ? "portfolioC" :
-                    estate.oma_salkku === "D" ? "portfolioD" : "portfolioDefault"
-              }`}>
-            <span className='estateName'>{estate.nimi}</span>
-            <span className='portfolio'>{estate.oma_salkku}</span>
-            <span className='estateNumber'>{estate.tasearvo[2023]}</span>
-            <span className='estateNumber'>{estate.pinta_ala}</span>
-            <span className='estateNumber'>{estate.tasearvo[2023]}</span>
-          </NavLink>
-        ))}
-      </div>
-      <div className="chartTower">
-        <div className='chartContainer'>
-          <DonutChart />
-        </div>
-        <div className='chartContainer'>
-          <PointsBarChart />
+
+        <div className="chartTower">
+          <div className="chartContainer">
+            <DonutChart />
+          </div>
+          <div className="chartContainer">
+            <PointsBarChart />
+          </div>
         </div>
       </div>
     </>
