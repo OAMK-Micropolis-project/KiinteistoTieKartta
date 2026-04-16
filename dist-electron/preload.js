a233d92163c1f3cdd4bc1a793853133b93ecefec
Andy <andy0ph@gmail.com>
2026-04-16 13:36:09 +0300

    deleted unnecessary files, while restoring crucial file back

--- dist-electron/preload.js
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-contextBridge.exposeInMainWorld('electronFs', {
-    readFile: async () => ipcRenderer.invoke('read-file'),
-    writeFile: async (data) => ipcRenderer.invoke('write-file', data),
-    openFile: () => ipcRenderer.invoke("open-file-dialog"),
-    saveFilePath: (path) => ipcRenderer.invoke("save-file-path", path),
-    getFilePath: () => ipcRenderer.invoke("get-file-path"),
-});
